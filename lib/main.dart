978056b240ec6d4afc305542facb8945e9df08f2
devanshu1005 <146869522+devanshu1005@users.noreply.github.com>
2025-03-10 00:49:24 +0530

    debug check remove

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => StockProvider()),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
